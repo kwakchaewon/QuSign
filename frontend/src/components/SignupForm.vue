4c24f37cd2f18ed37578b9c086a6e92d0a9d6b98
kwakchaewon <ksh03003@naver.com>
2026-06-04 10:50:19 +0900

    style: 약관 체크박스 문구 변경

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -131,7 +131,7 @@
         </svg>
       </span>
       <span>
-        <a href="#" @click.prevent="showTermsModal = true">이용약관 및 개인정보처리방침</a>에 동의해요
+        <a href="#" @click.prevent="showTermsModal = true">이용약관 및 개인정보처리방침</a>에 대해 확인과 동의가 필요해요.
       </span>
     </label>
 
